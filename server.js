846c42fc1a69e2e3294e25494287f0727f0bcd20
shubham dixit <shubhamd.monter@gmail.com>
2020-06-07 02:39:56 +0530

    path change 4

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => {
     console.log('Server is up!' + port);
