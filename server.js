3774494c6cdf7d7cf343b5b898fa387f236a2cea
shubham dixit <shubhamd.monter@gmail.com>
2020-06-07 02:44:09 +0530

    path change 5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+app.use(express.static(__dirname));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
