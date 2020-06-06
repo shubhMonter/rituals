577bff8fe6898e6203b9759fdf02ba737a067ac8
shubham dixit <shubhamd.monter@gmail.com>
2020-06-07 03:16:55 +0530

    path cahnges 9

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, () => {
     console.log('Server is up!' + port);
